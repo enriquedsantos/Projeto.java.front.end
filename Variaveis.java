48b48369e5787d2baf9de7df080a2a2294ecc53e
Enrique <enrique.d.santos@hotmail.com>
2024-07-04 15:15:38 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Variaveis {
+//declaração da classe variaveis 
+    public static void main(String [] args) throws Exception {
+        int varInt = 30; // declaração da variavel varInt do tipo int (inteiro) com valor recebido 30
+        System.out.println(varInt);
+    
+        Integer varInteger = 1994; // declaração da variavel varInt do tipo int (inteiro) com valor recebido 1994 
+        System.out.println(varInteger); 
+    
+        String varString = "Enrique"; // declaração da variavel varString do tipo String (texto) colocar aspas para saber que é String"" com valor recebido Enrique
+        System.out.println(varString);
+        
+        boolean varBoolean = true; // declaração da variavel varBoolean do tipo boolean com valor recebido true
+        System.out.println(varBoolean);
+    
+        byte varByte = 05; // declaração da variavel varByte do tipo byte dados binarios armazena numeros,objetos, imagens com valor recebido 05
+        System.out.println(varByte);
+    
+        char varChar = 'm'; // declaração da variavel varChar do tipo char com valor recebido 'm'
+        System.out.println(varChar);
+    
+        float varFloat = 96.00f; // declaração da variavel varFloat do tipo float com valor recebido 96.00f
+        //tipo nomeDaVariavel atribuição valor fechaLinhaDeCodigo
+        System.out.println(varFloat);
+        
+    }
+    
+}
